892b2cb3b1ba1e73f5a0736f0e2cbedb54bbd82e
Auyy <71812937+Auyy@users.noreply.github.com>
2021-04-20 13:17:58 +0700

    ex

---
+++ Lab01Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab01Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Please input number A + B ");
+            Console.WriteLine("------------------------- ");
+
+            Console.WriteLine("Input number A : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input number B : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Summary: " + (num1 + num2));
+            Console.ReadLine();
+
+        }
+    }
+}
